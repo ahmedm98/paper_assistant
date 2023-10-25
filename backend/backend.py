c838c105028d96e6eb4c1149d7fb103a52ee5dbb
@ahmed.mohamed <ahmed.mohamed@unternehmertum.de>
2023-10-25 12:48:29 +0200

    made the backend lighter

--- backend/backend.py
+++ backend/backend.py
@@ -3,12 +3,19 @@ from pydantic import BaseModel
 from utils import Similarity_calculator
 
 class User_input(BaseModel):
+    '''Represents the user input.
+
+    :param paragraph: user text input.
+    :type paragraph: str
+    :param k: the number of papers to be suggested.
+    :type k: int
+    '''
     paragraph : str
     k : int
 
 
 app = FastAPI()
-sim_calc=Similarity_calculator()
+sim_calc = Similarity_calculator()
 
 @app.post("/get_papers")
 def get_papers(input:User_input):
--- backend/utils.py
+++ backend/utils.py
@@ -7,27 +7,44 @@ from sentence_transformers import SentenceTransformer
 
 
 class Similarity_calculator():
-    def __init__(self):
+    '''Encodes the input text and retrieves the k most similar papers.'''
+
+    def __init__(self, data_path="updated_papers_dataset2.csv"):
+        '''Retrieves the database and initalize the LLM.
+        
+        :param data_path: path to the database.
+        :type data_path: str
+        '''
 
         self.model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
-        self.data_path = Path("updated_papers_dataset2.csv")
+        self.data_path = Path(data_path)
         self.dataset = pd.read_csv(self.data_path)
 
-    def get_top(self,paragraph,k):
+    def get_top(self, paragraph, k):
+        '''retrieve the k most similar papers to the input text.
 
-        para_embedding = list(self.model.encode(paragraph))
+        :param paragraph: user input text.
+        :type paragraph: str
+        :param k: number of papers to suggested.
+        :type k: int
+
+        :returns: the top k papers with their informations.
+        :rtype: dict
+        '''
+
+        input_embedding = list(self.model.encode(paragraph))
 
         scores = []
 
         for i in self.dataset.index:
 
             summary_embedding = literal_eval(self.dataset.iloc[i]["summary_embedding"])
-            score = self.get_score(para_embedding,summary_embedding)
+            score = self.get_score(input_embedding,summary_embedding)
 
             scores.append(score)
 
         scores_arr = np.array(scores)
-        top=scores_arr.argsort()[-k:][::-1]
+        top = scores_arr.argsort()[-k:][::-1]
 
 
         papers = [str(self.dataset.iloc[i]["paper"]) for i in top]
@@ -42,7 +59,19 @@ class Similarity_calculator():
                 "abstracts": abstract
                 }
 
+
     def get_score(self,emb1,emb2):
+        '''
+        Compute the cosine distance between two vectors.
+
+        :param emb1: first vector.
+        :type emb1: list
+        :param emb2: second vector.
+        :type emb2: list
+
+        :returns: cosine distance between emb1 and emb2.
+        :rtype: float
+        '''
         score = np.dot(emb1,emb2)/(norm(emb1)*norm(emb2))
 
         return score
